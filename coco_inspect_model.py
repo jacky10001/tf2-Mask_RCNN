3fcc2693caaf1cf3697301dfb70bfdb1878d2b5a
jacky10001 <50270079+jacky10001@users.noreply.github.com>
2020-12-12 01:50:44 +0800

    Update inspect code

--- coco_inspect_model.py
+++ coco_inspect_model.py
@@ -30,10 +30,8 @@ from mrcnn.samples import CocoDataset
 MODEL_DIR = 'log_coco'
 
 # Local path to trained weights file
-COCO_MODEL_PATH = "mask_rcnn_coco.h5"
-# Download COCO trained weights from Releases if needed
-if not os.path.exists(COCO_MODEL_PATH):
-    utils.download_trained_weights(COCO_MODEL_PATH)
+COCO_MODEL_PATH =\
+    os.path.join('pretrained_model','mask_rcnn_coco.h5')
 
 # MS COCO Dataset
 COCO_DIR = r'D:\YJ\MyDatasets\COCO\coco2014'
